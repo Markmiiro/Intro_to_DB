32891712694e5d5083ae20eaac7c1d8324782e9d
mark miiro <markmiiro77@gmail.com>
2025-10-19 20:47:19 +0300

    Add SQL script to insert single customer into alx_book_store

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Insert a single record into the customers table
+INSERT INTO customers (customer_id, name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
